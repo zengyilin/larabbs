cf61b38e972f0e7361b52cb2dbe5839a95fb5770
zengyilin <jason_tse55@163.com>
2019-01-04 17:23:01 +0800

    api/user

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\Api\UsersRequest;
 use App\Models\User;
+use App\Transformers\UserTransformer;
 use Illuminate\Support\Facades\Cache;
 
 class UsersController extends Controller
@@ -29,6 +30,19 @@ class UsersController extends Controller
 //        dd($user);
         Cache::forget($request->verification_key);
 
-        return $this->response->array(['code'=>0,'message'=>'注册成功']);
+        return $this->response->item($user, new UserTransformer())
+            ->setMeta([
+                'access_token' => \Auth::guard('api')->fromUser($user),
+                'token_type' => 'Bearer',
+                'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+            ])
+            ->setStatusCode(201);
     }
+
+    public function me()
+    {
+        return $this->response->item($this->user(), new UserTransformer());
+    }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Http\Request;
 
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1',['namespace'=>'App\Http\Controllers\Api'],function ($api){
+$api->version('v1',['namespace'=>'App\Http\Controllers\Api','middleware'=>'serializer:array'],function ($api){
 
     $api->group([
         'middleware'=>'api.throttle',
@@ -37,24 +37,40 @@ $api->version('v1',['namespace'=>'App\Http\Controllers\Api'],function ($api){
 //    用户注册
         $api->post('users','UsersController@store')
             ->name('api.users.store');
-    });
 
 //    第三方登录
-    $api->post('socials/{social_type}/authorizations','AuthorizationsController@socialStore')
-        ->name('api.socials.authorizations.store');
+        $api->post('socials/{social_type}/authorizations','AuthorizationsController@socialStore')
+            ->name('api.socials.authorizations.store');
 
 //    登录
-    $api->post('authorizations', 'AuthorizationsController@store')
-        ->name('api.authorizations.store');
+        $api->post('authorizations', 'AuthorizationsController@store')
+            ->name('api.authorizations.store');
 
 //    刷新token
-    $api->put('authorizations/current','AuthorizationsController@update')
-    ->name('api.authorizations.update');
+        $api->put('authorizations/current','AuthorizationsController@update')
+            ->name('api.authorizations.update');
 
 //    删除token
-    $api->delete('authorizations/current','AuthorizationsController@destroy')
-        ->name('api.authorizations.destroy');
+        $api->delete('authorizations/current','AuthorizationsController@destroy')
+            ->name('api.authorizations.destroy');
+    });
 
+
+
+    $api->group([
+        'middleware'=>'api.throttle',
+        'limit'=>config('api.rate_limits.access.limit'),
+        'expires'=>config('api.rate_limits.access.expires')
+    ],function ($api){
+//        游客可以访问的接口
+
+
+//        需要 token验证才能访问的接口
+        $api->group(['middleware'=>'api.auth'],function ($api){
+            $api->get('user','UsersController@me')->name('api.user.show');
+        });
+
+    });
 
 });
 
